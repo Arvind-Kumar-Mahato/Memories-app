2f81686095789c20314aff6f82fd73e337af66d7
Arvind-Kumar-Mahato <93374171+Arvind-Kumar-Mahato@users.noreply.github.com>
2022-09-28 22:21:07 +0530

    working on ui part

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+import memories from './Images/memories.png'
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Container maxWidth ='lg'>
+    <AppBar position= "static" color= 'inherit'>
+    <Typography variant="h2" align="center">
+     Memories
+    </Typography>
+    <img src = {memories} alt = "memories" height= "200"/>
+
+    </AppBar>
+
+    </Container>
   );
 }
 
