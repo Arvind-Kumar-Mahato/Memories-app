125ba4641a620e861598aa6f251455fcb3d95fb6
Arvind-Kumar-Mahato <93374171+Arvind-Kumar-Mahato@users.noreply.github.com>
2022-09-29 18:38:47 +0530

    working on form component

--- client/src/Components/Forms/Form.jsx
+++ client/src/Components/Forms/Form.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React,{useState} from 'react'
 import useStyles from "./styles";
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 const Form = () => {
+
+  const [postData, setPostData] = useState({creator: "", title:"", message:"", tags:"", selectedFile:"" })
    const classes = useStyles();
 
    const handlesubmit=()=>{
@@ -10,8 +12,16 @@ const Form = () => {
   return (
     <Paper className={classes.paper}>
      <form autoComplete='off' noValidate className={classes.form} onSubmit={handlesubmit}>
-     
+     <Typography variant='h6'>
+      Creating a Memory
+     </Typography>
+     <TextField name = "creator" 
+      varient = "outlined"
+      label = "Creator"
+      value={postData.creator}
+      onChange = {(e)=>setPostData({...postData, creator:e.target.value})}
 
+     />
      </form>
     </Paper>
   )
