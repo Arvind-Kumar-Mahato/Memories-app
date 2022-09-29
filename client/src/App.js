48783b6848d25ce5be90abaf42726712bd54f131
Arvind-Kumar-Mahato <93374171+Arvind-Kumar-Mahato@users.noreply.github.com>
2022-09-29 14:26:36 +0530

    working on form part

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,37 @@
 import React from "react";
 import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
 import memories from './Images/memories.png'
+import Posts from "./Components/Posts/Posts";
+import Form from "./Components/Forms/Form";
+import useStyles from "./Styles"
 function App() {
+  const classes = useStyles();
   return (
-    <Container maxWidth ='lg'>
-    <AppBar position= "static" color= 'inherit'>
-    <Typography variant="h2" align="center">
-     Memories
-    </Typography>
-    <img src = {memories} alt = "memories" height= "200"/>
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className = {classes.heading} variant="h2" align="center">
+          Memories
+        </Typography>
+        <img className = {classes.image} src={memories} alt="memories" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts/>
+            </Grid>
 
-    </AppBar>
-
+            <Grid item xs={12} sm={4}>
+              <Form/>
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
     </Container>
   );
 }
--- client/src/Components/Forms/Form.jsx
+++ client/src/Components/Forms/Form.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Form = () => {
   return (
     <div>
-      
+      <h1>From</h1>
     </div>
   )
 }
