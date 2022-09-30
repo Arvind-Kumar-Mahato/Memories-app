d59fd7461354b1e3bb552445685a9a7dd7b6454a
Arvind-Kumar-Mahato <93374171+Arvind-Kumar-Mahato@users.noreply.github.com>
2022-09-30 12:33:43 +0530

    form ui complete

--- client/src/Components/Forms/Form.jsx
+++ client/src/Components/Forms/Form.jsx
@@ -1,30 +1,103 @@
 import React,{useState} from 'react'
 import useStyles from "./styles";
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
+import FileBase from 'react-file-base64';
+import {useDispatch} from 'react-redux';
+import { createPost } from '../../actions/posts';
 const Form = () => {
 
   const [postData, setPostData] = useState({creator: "", title:"", message:"", tags:"", selectedFile:"" })
    const classes = useStyles();
+    const dispatch = useDispatch();
+   const handlesubmit=(e)=>{
+    e.preventDefault();
+    dispatch(createPost(postData));
+   }
 
-   const handlesubmit=()=>{
+   const clear = ()=>{
 
    }
   return (
     <Paper className={classes.paper}>
-     <form autoComplete='off' noValidate className={classes.form} onSubmit={handlesubmit}>
-     <Typography variant='h6'>
-      Creating a Memory
-     </Typography>
-     <TextField name = "creator" 
-      varient = "outlined"
-      label = "Creator"
-      value={postData.creator}
-      onChange = {(e)=>setPostData({...postData, creator:e.target.value})}
+      <form
+        autoComplete="off"
+        noValidate
+        className={`${classes.root} ${classes.form}`}
+        onSubmit={handlesubmit}
+      >
+        <Typography variant="h6">Creating a Memory</Typography>
+        <TextField
+          name="creator"
+          varient="outlined"
+          fullWidth
+          label="Creator"
+          value={postData.creator}
+          onChange={(e) =>
+            setPostData({ ...postData, creator: e.target.value })
+          }
+        />
+        <TextField
+          name="title"
+          varient="outlined"
+          fullWidth
+          label="Title"
+          value={postData.title}
+          onChange={(e) => setPostData({ ...postData, title: e.target.value })}
+        />
+        <TextField
+          name="message"
+          varient="outlined"
+          fullWidth
+          label="Message"
+          multiline
+          rows={4}
+          value={postData.message}
+          onChange={(e) =>
+            setPostData({ ...postData, message: e.target.value })
+          }
+        />
 
-     />
-     </form>
+        <TextField
+          name="tags"
+          varient="outlined"
+          fullWidth
+          label="Tags (coma separated)"
+          value={postData.tags}
+          onChange={(e) =>
+            setPostData({ ...postData, tags: e.target.value.split(",") })
+          }
+        />
+        <div className={classes.fileInput}>
+          <FileBase
+            type="file"
+            multiple={false}
+            onDone={({ base64 }) =>
+              setPostData({ ...postData, selectedFile: base64 })
+            }
+          />
+        </div>
+        <Button
+          className={classes.buttonSubmit}
+          variant="contained"
+          color="primary"
+          size="large"
+          type="submit"
+          fullWidth
+        >
+          Submit
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          size="small"
+          onClick={clear}
+          fullWidth
+        >
+          Clear
+        </Button>
+      </form>
     </Paper>
-  )
+  );
 }
 
 export default Form
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,10 +1,18 @@
-export default (posts = [], action) =>{
-    switch(action.type){
-        case 'Fetch_ALL':
-            return action.payload;
-            case'CREATE':
-            return posts;
-            default:
-                return posts;
-    }
-}
+import {
+  FETCH_ALL,
+  CREATE,
+  UPDATE,
+  DELETE,
+  LIKE,
+} from "../constants/actionTypes"
+
+export default (posts = [], action) => {
+  switch (action.type) {
+    case "Fetch_ALL":
+      return action.payload;
+    case "CREATE":
+      return [...posts, action.payload];
+    default:
+      return posts;
+  }
+};
