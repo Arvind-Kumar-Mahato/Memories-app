d7179c852c1ebfcc41d0e1c57157a5445bd46e47
Arvind-Kumar-Mahato <93374171+Arvind-Kumar-Mahato@users.noreply.github.com>
2022-10-01 23:02:47 +0530

    working on post

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,22 +1,31 @@
 import PostMessage from "../models/postMessage.js";
 
-export const getPosts = async (req,res)=>{
-    try{
-    const postMessage = await PostMessage.find();
-      res.status(200).json(postMessage);
-    }
-    catch(error){
-     res.status(404).json({message:error.message});
-    }
-}
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
 
-export const createPost = async (req,res)=>{
-    const post = req.body;
-    const newPost = new PostMessage(post);
-    try{
-     await newPost.save();
-     res.status(201).json(newPost);
-    }catch(error){
-    res.status(409).json({message:error.message});
-    }
-}
+    res.status(200).json(postMessages);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+export const createPost = async (req, res) => {
+  const { title, message, selectedFile, creator, tags } = req.body;
+
+  const newPostMessage = new PostMessage({
+    title,
+    message,
+    selectedFile,
+    creator,
+    tags,
+  });
+
+  try {
+    await newPostMessage.save();
+
+    res.status(201).json(newPostMessage);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
