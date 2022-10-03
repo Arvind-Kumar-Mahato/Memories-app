64a7d88e42643ba5fcf7de6fffdd3742ec6c5409
Arvind-Kumar-Mahato <93374171+Arvind-Kumar-Mahato@users.noreply.github.com>
2022-10-03 20:25:33 +0530

    post is shown on page

--- client/src/Components/Posts/Post/Post.jsx
+++ client/src/Components/Posts/Post/Post.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
 import useStyles from "./styles";
-const Post = () => {
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import DeleteIcon from '@material-ui/icons/Delete';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import moment from 'moment';
+import { Card, CardActions, CarContent,CardMedia,Button, Typography } from '@material-ui/core';
+const Post = ({post}) => {
    const classes = useStyles();
   return (
-    <div>
-     <h1>Post</h1> 
-     <h2>Post</h2>
+    <Card className = {classes.card}>
+    <CardMedia className={classes.media} image={post.selectedFile } title = {post.title}/>
+    <div className= {classes.overlay}>
+    <Typography variant ="h6">
+    {post.creator}
+    </Typography>
+    <Typography variant = "body2">
+  {moment(post.createdAt).fromNow()}
+    </Typography>
     </div>
+    <div className={classes.overlay2}>
+    <Button style={{color:"white"}} size ="small" ><MoreHorizIcon fontSize="default"/></Button>
+      
+    </div>
+    <div className={classes.details}>
+      <Typography variant="body2" color="textSecondary" component ="h2" >
+
+      </Typography>
+    </div>
+    </Card>
   )
 }
 
--- client/src/Components/Posts/Posts.jsx
+++ client/src/Components/Posts/Posts.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import Post from './Post/Post';
+
 import useStyles from "./styles";
 import {useSelector} from 'react-redux';
-
+import {Grid,CircularProgress} from '@material-ui/core'
+import Post from './Post/Post';
 const Posts = () => {
 
   
@@ -11,10 +12,17 @@ const Posts = () => {
    const classes = useStyles();
    console.log(posts);
   return (
-    <div>
-     <h1>Posts</h1>
-   <Post/>
-    </div>
+    !posts.length ? <CircularProgress/> : (
+      <Grid className={classes.conainer} container alignItems='stretch' spacing = {3} >
+      {posts.map((post)=>(
+       <Grid key = {post._id} item xs={12} sm={6} md={6}>
+       <Post post = {post}/>
+
+       </Grid>
+      ))}
+
+      </Grid>
+    )
   )
 }
 
