3bedd6f0e7eb4ee62bb42a941f0bd547e06004b1
Arvind-Kumar-Mahato <93374171+Arvind-Kumar-Mahato@users.noreply.github.com>
2022-10-14 11:20:59 +0530

    working on navbar

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -3,20 +3,21 @@ import {
   CREATE,
   UPDATE,
   DELETE,
-  LIKE,
+
 } from "../constants/actionTypes"
 
 export default (posts = [], action) => {
   switch (action.type) {
-   case 'DELETE':
-    return posts.filter((post)=>post.id != action.payload);
-    case'UPDATE':
-    case'Like':
-    return posts.map((post)=>post._id === action.payload._id ? action.payload : post);
+    case DELETE:
+      return posts.filter((post) => post.id != action.payload);
+    case UPDATE:
+      return posts.map((post) =>
+        post._id === action.payload._id ? action.payload : post
+      );
 
-    case "Fetch_ALL":
+    case FETCH_ALL:
       return action.payload;
-    case "CREATE":
+    case CREATE:
       return [...posts, action.payload];
     default:
       return posts;
